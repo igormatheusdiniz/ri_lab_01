e51618879a9904332194a554bc51a44e1a1894e4
igormatheusdiniz <igor.pinheiro@ccc.ufcg.edu.br>
2019-04-03 23:03:33 -0300

    primeiros passos no spider

--- ri_lab_01/spiders/brasil_elpais.py
+++ ri_lab_01/spiders/brasil_elpais.py
@@ -18,14 +18,29 @@ class BrasilElpaisSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
+
+        for href in response.css("h2.articulo-titulo a::attr(href)"):
+            yield response.follow(href, self.parse_content, meta={'url': response.url})
+                    
+                
+
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
-        #
-        #
-        #
+
+    def parse_content(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'title': extract_with_css('h1.articulo-titulo::text'),
+            'sub-title': extract_with_css('h2.articulo-subtitulo::text'),
+            'author': extract_with_css('span.autor-nombre  a::text'),
+            'date': extract_with_css('time::attr(datetime)'),
+            'url': response.url,
+            'section': response.meta['url'].split('/')[-2],
+            'text': extract_with_css('div.articulo-cuerpo p::text')
+        }
+        
